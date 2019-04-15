ed4475042bb186dd0e476d497e4876508262b920
Rood RF <roderick_ch@hotmail.com>
2019-04-14 21:33:49 -0500

    Firts Commit

---
+++ app/src/main/java/com/universe/rodrf/locationhistory/Activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.universe.rodrf.locationhistory.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.universe.rodrf.locationhistory.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
